1af0ca4d28af1974f242bdd8ac17f7878f2d1ced
Davecastle10 <91599399+Davecastle10@users.noreply.github.com>
2022-09-18 17:52:34 +0000

    made a heart

--- main.py
+++ main.py
@@ -15,17 +15,54 @@ screen.setup(width = 500, height = 500)
 t = turtle.Turtle() 
 t.shape("turtle") 
 t.speed(0) # 1:slowest, 3:slow, 5:normal, 10:fast, 0:fastest 
-t.pencolor("red") 
-t.stamp()# 
-x = random.randint(1,200) 
-y = random.randint(1,200) 
-turns = 0 
-placeh = 0 
-for i in range(75): 
-  t.forward(100) 
-  t.right(200) 
-  turns +=1 
-x_pos = t.pos()[0]
-y_pos = t.pos()[1]
-print(x_pos)
+t.pencolor("blue") 
+t.fillcolor('red')
+#t.stamp()#creates an image of turtle(in this case at the start pos)
+
+#make a heart
+t.penup()
+t.left(180)
+t.forward(115)
+t.right(90) 
+t.pendown()
+t.begin_fill()
+for i in range (180):
+  t.forward(1)
+  t.right(1)
+t.right(180)
+for i in range (180):
+  t.forward(1)
+  t.right(1)
+t.right(30)
+t.forward(230)
+t.right(120)
+t.forward(230)
+t.end_fill()
+t.penup()
+t.forward(30)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#make a square that is in a radius of 100 from start pos
+''' makse a square with side length 200
+t.begin_fill()
+t.forward(100)
+t.right(90)
+for i in range(3): 
+  t.forward(200) 
+  t.right(90) 
+t.forward(100)
+t.end_fill()
 turtle.done()
+'''
--- turtle_test.py
+++ turtle_test.py
@@ -1,41 +1,7 @@
-#this is me pplaying with the turtle function
-#it runs in repl.it but not gitpod
-#importing relevant packages
-import turtle 
-import random 
-
-#making a screen for turtle to draw onto
-screen = turtle.Screen()
-screen.title("Playing with Turtle")
-screen.bgcolor('lightblue') #background is lightblue
-screen.setup(width = 500, height = 500)
-
-
-
-t = turtle.Turtle() 
-
-t.shape("turtle") 
-
-t.speed(0) # 1:slowest, 3:slow, 5:normal, 10:fast, 0:fastest 
-
-t.color("red") 
-
-t.stamp()# 
-
 x = random.randint(1,200) 
-
 y = random.randint(1,200) 
-
 turns = 0 
-
 placeh = 0 
-
-for i in range(75): 
-
-  t.forward(100) 
-
-  t.right(200) 
-
-  turns +=1 
-
-turtle.done()
+x_pos = t.pos()[0]
+y_pos = t.pos()[1]
+print(x_pos)
