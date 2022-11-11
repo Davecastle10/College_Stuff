6fe921d87b3fd1516009632d35dbde0867116b0f
Davecastle10 <davecastlejones10@gmail.com>
2022-11-11 13:19:13 +0000

    did some array questions

--- array_tasks/is_array_ordered_task.py
+++ array_tasks/is_array_ordered_task.py
@@ -1,4 +1,11 @@
 # a function to check if an array is ordered
 
 def is_ordered(arr):
-    dsfg
+    for i in range(len(arr)-1):
+        if arr[i] > arr[i+1]:
+            return False
+        else:
+            return True
+
+test_arr = [1,2,4,7,2,8,9,12,23,3,5]
+print(is_ordered(test_arr))
