798c4bd33ce69dd5723cd30b6dd5a1edd4549054
Davecastle10 <davecastlejones10@gmail.com>
2022-11-24 13:41:33 +0000

    changed the algorithm fo linear search

--- searching algorithms/linear.py
+++ searching algorithms/linear.py
@@ -1,6 +1,6 @@
 #returns true if the entered item is in the inputted list could be an array though
 
-def search(list, item, index):	
+def linear_search(list, item, index):	
     """ parameters, the list, the item, and whether you want the index ( True) 
     or just to know if it's there (False) """
     for i in range(len(list)):
@@ -10,5 +10,10 @@ def search(list, item, index):
             else:
                 return True
     return False
+    """  if you don't want to finish the function straight away use the 'pass' command 
+    at the end of the function so it doesnt get excecuted"""
 
-print(search([1,2,3,4,5], 3, False))
+test_list = [1,2,3,4,5,23,45,9]
+
+print(linear_search(test_list, 3, False))
+print("index", linear_search(test_list, 3, True))
