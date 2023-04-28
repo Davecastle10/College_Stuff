a92bbab44e876226a06174ae88811bd38fbd7921
Davecastle10 <91599399+Davecastle10@users.noreply.github.com>
2023-04-28 11:07:03 +0100

    Update is_array_ordered_task.py
    Changed indentation to try and fix the program

--- array_tasks/is_array_ordered_task.py
+++ array_tasks/is_array_ordered_task.py
@@ -4,8 +4,8 @@ def is_ordered(arr):
     for i in range(len(arr)):
         if arr[i+1] < arr[i]:
             return False
-        else:
-            return True
+        
+    return True# moved this line to be in line with the start of the for loop instead of under an else: statement by the if: statement to see if it runs hopefully. 
 
 test_arr = [1,2,4,7,2,8,9,12,23,3,5]
 print(is_ordered(test_arr))
