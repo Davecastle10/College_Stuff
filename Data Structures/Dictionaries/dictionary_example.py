3da4e35824c92c8db64cc6f36b432447f642aaab
Davecastle10 <davecastlejones10@gmail.com>
2023-05-04 14:20:56 +0000

    added some notes on dictionaries

---
+++ Data Structures/Dictionaries/dictionary_example.py
@@ -0,0 +1,11 @@
+# This is an example of a dictionary that stores the test results of some students
+
+results = {"Detra": 17,
+       "Nova": 70,
+       "Dave": 71,
+       "John": 63,
+       "Chris": 29,
+       "Elon": 28}
+
+# the green text on the left of each : is the key for the pair
+# and the pink text on the left of each pair is the value
