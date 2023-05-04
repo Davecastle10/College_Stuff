6d0a9e15f957f6affadc1b21a8b9c223dd81ab0f
Davecastle10 <davecastlejones10@gmail.com>
2023-05-04 14:43:08 +0000

    did some dictionary handling

--- Data Structures/Dictionaries/dictionary_example.py
+++ Data Structures/Dictionaries/dictionary_example.py
@@ -1,4 +1,5 @@
-# This is an example of a dictionary that stores the test results of some students
+""" This is an example of a dictionary that stores the test results of some students
+"""
 
 results = {"Detra": 17,
        "Nova": 70,
@@ -9,3 +10,15 @@ results = {"Detra": 17,
 
 # the green text on the left of each : is the key for the pair
 # and the pink text on the left of each pair is the value
+
+
+""" This is an example of retriving data from a dictionary and using it to work out the class average score as an integer
+"""
+
+#results_key_list = list(results.keys())[1]
+average = 0
+total = 0
+for i in range(len(results)):
+    total = total + results[list(results.keys())[i]]
+average = int(total/len(results))
+print(average)
