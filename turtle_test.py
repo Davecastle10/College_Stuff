f79009bc856d34d37b8fc7a72d668ad62521a483
Dave Jones <davecastlejones10@gmail.com>
2024-03-09 16:01:05 +0000

    syncing with github

--- turtle_test.py
+++ turtle_test.py
@@ -1,7 +1,5 @@
-x = random.randint(1,200) 
-y = random.randint(1,200) 
-turns = 0 
-placeh = 0 
-x_pos = t.pos()[0]
-y_pos = t.pos()[1]
-print(x_pos)
+
+list1 = [1,2,3]
+
+for i in range(len(list1)):
+    print(list1[i]+10)
