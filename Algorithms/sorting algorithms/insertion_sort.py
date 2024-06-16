7eed383b9d692d0aaf7b351904cbedaf7a99fe79
Davecastle10 <davecastlejones10@gmail.com>
2024-06-16 16:33:07 +0000

    jgjg

--- Algorithms/sorting algorithms/insertion_sort.py
+++ Algorithms/sorting algorithms/insertion_sort.py
@@ -1,21 +1,22 @@
 """ My Attempt at making an insertion sort
 
-
+    It works
 
 
     Pseudocode:
 
-        Start with leftmost item,
-        copy it into temp/current itme variable
-        iterate through array, moving each item greater than the current itme 1 to the left, 
+        Start with rigthmost item of the selected part of the liost,
+        copy it into current itme variable
+        iterate through array, moving each item greater than the current itme 1 to the right, 
         until a smaller itme is found, 
-        at which point the current itme is inserted at the index 1 to the left of the mslaller item
+        at which point the current itme is inserted at the index 1 to the right of the mslaller item
 """
 
 def insertion_sort(arr_to_sort):
     for index in range(len(arr_to_sort)):
         current_item = arr_to_sort[index]
         new_index = index
+
         while new_index > 0 and arr_to_sort[new_index - 1] > current_item:
             arr_to_sort[new_index] = arr_to_sort[new_index - 1]
             new_index = new_index - 1
